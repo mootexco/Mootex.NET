59d40571229c9ebfdbc47c4c4f5504762d0f7668
Jose J. Fernández <josejfernandez@users.noreply.github.com>
2021-06-12 22:33:27 +0200

    Fix encrypt and decrypt with AES

--- src/Mootex.Encryption/AES/AESEncryption.cs
+++ src/Mootex.Encryption/AES/AESEncryption.cs
@@ -20,112 +20,71 @@ namespace Mootex.Encryption.AES {
 
     public sealed class AESEncryption {
 
-        public static byte[] Encrypt(string data, byte[] key, byte[] iv) {
+        private readonly string key;
 
-            if (data == null) {
-                throw new ArgumentNullException(nameof(data));
+        public AESEncryption(string encryptionKey) {
+
+            if (string.IsNullOrEmpty(encryptionKey)) {
+                throw new ArgumentNullException(nameof(encryptionKey));
             }
 
-            if (data.Length <= 0) {
-                throw new ArgumentException("Invalid data length.", nameof(data));
-            }
-
-            if (key == null) {
-                throw new ArgumentNullException(nameof(key));
-            }
-
-            if (!CheckKey(key)) {
-                throw new ArgumentException("Invalid key length.", nameof(key));
-            }
-
-            if (iv == null) {
-                throw new ArgumentNullException(nameof(iv));
-            }
-
-            if (iv.Length != 16) {
-                throw new ArgumentException("Invalid iv length.", nameof(iv));
-            }
-
-            byte[] encrypted;
-
-            using (Aes aes = Create(key, iv)) {
-
-                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-                using (MemoryStream msEncrypt = new MemoryStream()) {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write)) {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt)) {
-                            swEncrypt.Write(data);
-                        }
-                        encrypted = msEncrypt.ToArray();
-                    }
-                }
-
-            }
-
-            return encrypted;
+            this.key = encryptionKey;
 
         }
 
-        public static string Decrypt(byte[] data, byte[] key, byte[] iv) {
+        private AesManaged CreateAes() {
 
-            if (data == null) {
-                throw new ArgumentNullException(nameof(data));
-            }
-
-            if (data.Length <= 0) {
-                throw new ArgumentException("Invalid data length.", nameof(data));
-            }
-
-            if (key == null) {
-                throw new ArgumentNullException(nameof(key));
-            }
-
-            if (!CheckKey(key)) {
-                throw new ArgumentException("Invalid key length.", nameof(key));
-            }
-
-            if (iv == null) {
-                throw new ArgumentNullException(nameof(iv));
-            }
-
-            if (iv.Length != 16) {
-                throw new ArgumentException("Invalid iv length.", nameof(iv));
-            }
-
-            string plaintext = string.Empty;
-
-            using (Aes aes = Create(key, iv)) {
-
-                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-                using (MemoryStream msDecrypt = new MemoryStream(data)) {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read)) {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt)) {
-                            plaintext = srDecrypt.ReadToEnd();
-                        }
-                    }
-                }
-
-            }
-
-            return plaintext;
-
-        }
-
-        private static Aes Create(byte[] key, byte[] iv) {
-
-            Aes aes = Aes.Create();
+            AesManaged aes = new AesManaged();
             aes.Mode = CipherMode.CBC;
-            aes.Key = key;
-            aes.IV = iv;
+            aes.Padding = PaddingMode.PKCS7;
+
+            Rfc2898DeriveBytes source = new(this.key, new byte[8]);
+            aes.Key = source.GetBytes(aes.KeySize / 8);
+            aes.IV = source.GetBytes(aes.BlockSize / 8);
 
             return aes;
 
         }
 
-        private static bool CheckKey(byte[] key) {
-            return Aes.Create().ValidKeySize(key.Length * 8);
+        public string Encrypt(string data) {
+
+            if (string.IsNullOrEmpty(data)) {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            using (AesManaged aes = this.CreateAes()) {
+                using (ICryptoTransform encryptor = aes.CreateEncryptor()) {
+                    using (MemoryStream msEncrypt = new()) {
+                        using (CryptoStream csEncrypt = new(msEncrypt, encryptor, CryptoStreamMode.Write)) {
+                            using (StreamWriter swEncrypt = new(csEncrypt)) {
+                                swEncrypt.Write(data);
+                            }
+                        }
+                        return Convert.ToBase64String(msEncrypt.ToArray());
+                    }
+                }
+            }
+
+        }
+
+        public string Decrypt(string data) {
+
+            if (string.IsNullOrEmpty(data)) {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            using (AesManaged aes = this.CreateAes()) {
+                using (ICryptoTransform decryptor = aes.CreateDecryptor()) {
+                    using (MemoryStream msDecrypt = new(Convert.FromBase64String(data))) {
+                        using (CryptoStream csDecrypt = new(msDecrypt, decryptor, CryptoStreamMode.Read)) {
+                            using (StreamReader srDecrypt = new(csDecrypt)) {
+                                return srDecrypt.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+
         }
 
     }
